5a36e0c1021c053e5b02c7cffb4312de0592ae78
Soap1196 <Jdmack007@aol.com>
2022-10-17 14:07:37 -0500

    10/17/2022

--- src/Control.java
+++ src/Control.java
@@ -33,6 +33,29 @@ import javafx.stage.Stage;
 
 public class Control implements Initializable{
 
+    public Image drone;
+
+    interface items {
+        public void showItemDetails();
+    }
+
+    class component implements items {
+        public String[] obj = new String[30];
+        public String name;
+        public long price;
+        public long xCoordinate;
+        public long yCoordinate;
+        public long length;
+        public long width;
+        public long height;
+
+        @Override
+        public void showItemDetails()
+        {
+            System.out.println("Placeholder");
+        }
+
+    }
     
 
 
