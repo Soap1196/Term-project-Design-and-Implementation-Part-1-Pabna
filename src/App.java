7b4d4df34a249a376af37cc35af7fbede76eb013
Soap1196 <Jdmack007@aol.com>
2022-10-19 12:15:15 -0500

    Singleton Addition

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.SequentialTransition;
@@ -14,25 +15,111 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class App extends Application {
+class itemList extends Stage{ 
+        
+    public static itemList itemObject; 
+  
+    
+    public String s; 
+  
+    
+    private itemList() 
+    { 
+         
+    } 
+  
+    
+    public static itemList getInstance() 
+    { 
+        if (itemObject == null) {
+            itemObject = new itemList(); 
+        }
+        return itemObject;
+  
+        
+    } 
 
     
+    
+    
+} 
+
+interface items {
+    public void showItemDetails();
+}
+
+class component implements items {
+    public String[] obj = new String[30];
+    public String name;
+    public long price;
+    public long xCoordinate;
+    public long yCoordinate;
+    public long length;
+    public long width;
+    public long height;
 
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
-        Scene App = new Scene(root);
-        stage.setScene(App);
-        stage.show();
+    public void showItemDetails()
+    {
+        System.out.println("Placeholder for component");
     }
 
-    public static void main(String[] args) {
-        launch(args);
+}
+
+class Leaf implements items{
+    public String name;
+    public long price;
+    public long xCoordinate;
+    public long yCoordinate;
+    public long length;
+    public long width;
+    public long height;
+
+    @Override
+    public void showItemDetails()
+    {
+        System.out.println("Placeholder fo component");
     }
+}
+
+public class App extends Application
+{
+    public static void main(String[] args)
+        {
+            Application.launch(args);
+        }
+
+    @Override
+	public void start(Stage stage) throws InterruptedException
+	{
+        itemList IL;
+        IL= itemList.getInstance();
+        // Create the VBox
+		VBox root = new VBox();
+		// Set Spacing to 10 pixels
+		root.setSpacing(10);
+		// Set the Size of the VBox
+		root.setPrefSize(800, 600);
+		// Set the Style-properties of the VBox
+		root.setStyle("-fx-padding: 10;" +
+				"-fx-border-style: solid inside;" +
+				"-fx-border-width: 2;" +
+				"-fx-border-insets: 5;" +
+				"-fx-border-radius: 5;" +
+				"-fx-border-color: transparent;");
+
+		// Create the Scene
+		Scene scene = new Scene(root);
+		// Add the Scene to the Stage
+		IL.setScene(scene);
+		// Set the Title of the Stage
+		IL.setTitle("A JavaFX Animation Example With Button");
+		// Display the Stage
+		IL.show();
+        
+    }
+
+    
+    
 }
