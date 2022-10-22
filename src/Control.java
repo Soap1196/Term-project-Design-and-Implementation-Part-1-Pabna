063f17f48f648160c19833bf40daa4d5d217b713
tannertavel <tannertavel@gmail.com>
2022-10-22 16:35:58 -0500

    Altered Control
    Added some of the FXML skeleton

--- src/Control.java
+++ src/Control.java
@@ -1,68 +1,54 @@
-import javafx.fxml.Initializable;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.Button;
-import javafx.collections.ObservableList;
-import javafx.scene.control.ListView;
-import java.util.Random;
-import javafx.scene.control.ChoiceBox;
-import java.math.*;
-import javafx.collections.FXCollections;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.SequentialTransition;
-import javafx.animation.Timeline;
-import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
+import javafx.scene.control.TreeView;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class Control implements Initializable{
+public class Control {
 
-    public Image drone;
+    @FXML
+    private Button Delete;
 
-    interface items {
-        public void showItemDetails();
+    @FXML
+    private Button Rename;
+
+    @FXML
+    private Button addItemContainer;
+
+    @FXML
+    private Button addItems;
+
+    @FXML
+    private Button changeDimensions;
+
+    @FXML
+    private Button changeLocation;
+
+    @FXML
+    private Button changePrices;
+
+    @FXML
+    private ImageView grid;
+
+    @FXML
+    private Button moveDrone;
+
+    @FXML
+    private TreeView<?> treeview;
+
+    @FXML
+    void addItemContainer(ActionEvent event) {
+
     }
 
-    class component implements items {
-        public String[] obj = new String[30];
-        public String name;
-        public long price;
-        public long xCoordinate;
-        public long yCoordinate;
-        public long length;
-        public long width;
-        public long height;
-
-        @Override
-        public void showItemDetails()
-        {
-            System.out.println("Placeholder");
-        }
+    @FXML
+    void additem(ActionEvent event) {
 
     }
-    
 
+    @FXML
+    void moveDrone(ActionEvent event) {
 
-    
+    }
 
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-        
-	}
 }
